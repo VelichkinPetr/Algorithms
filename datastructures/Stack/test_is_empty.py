d35505e8ebfa4b8be3d1a687cc6cca88fe0850a2
Petr Velichkin <pyo7578@yandex.ru>
2024-12-21 18:50:45 +0300

    feat(tests Stack): Переработал тесты

--- datastructures/Stack/test_is_empty.py
+++ datastructures/Stack/test_is_empty.py
@@ -3,14 +3,24 @@ from Stack import Stack
 
 @pytest.mark.parametrize('input_data, expected',
                          [
-                             (0, True),
-                             (1, False),
                              (2, False),
                              (3, False),
                              (4, False)
                          ])
 
-def test_is_empty(input_data,expected):
+def test_positive(input_data,expected):
+    s = Stack()
+    for i in range(0,input_data):
+        s.push(i)
+    assert s.is_empty() == expected
+
+@pytest.mark.parametrize('input_data, expected',
+                         [
+                             (0, True),
+                             (1, False)
+                         ])
+
+def test_bound(input_data,expected):
     s = Stack()
     for i in range(0,input_data):
         s.push(i)
--- datastructures/Stack/test_push.py
+++ datastructures/Stack/test_push.py
@@ -18,11 +18,12 @@ def test_positive(input_data,expected):
 
 @pytest.mark.parametrize('input_data, expected',
                          [
-                             (1, [1])
+                             (1, [1]),
+                             ('', ['']),
+                             ([], [[]])
                          ])
 
 def test_bound(input_data,expected):
     s = Stack()
-    for i in range(1,input_data+1):
-        s.push(i)
+    s.push(input_data)
     assert s.print_Stack() == expected
